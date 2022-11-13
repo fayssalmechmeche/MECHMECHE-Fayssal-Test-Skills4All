46b6faed3517cf8206ba056acd8b1097be9a367e
fayssal <fayssalmechmeche.pro@gmail.com>
2022-11-14 00:16:50 +0100

    delete function

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -53,4 +53,50 @@ class CarController extends AbstractController
             'form' => $form
         ]);
     }
+
+    #[Route('/admin', name: 'app_car_admin')]
+    public function admin(Request $request, PaginatorInterface $paginator): Response
+    {
+
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
+        $form = $form->handleRequest($request);
+        $cars = $paginator->paginate(
+            $this->entityManager->getRepository(Car::class)->findAll(), /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            5 /*limit per page*/
+        );
+        if ($form->isSubmitted() && $form->isValid()) {
+            //$cars = $this->entityManager->getRepository(Car::class)->findWithSearch($search);
+            $cars = $paginator->paginate(
+                $this->entityManager->getRepository(Car::class)->findWithSearch($search), /* query NOT result */
+                $request->query->getInt('page', 1), /*page number*/
+                5 /*limit per page*/
+            );
+        }
+        return $this->renderForm('car/crud.html.twig', [
+            'cars' => $cars,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/admin/create', name: 'app_car_create')]
+    public function create(Request $request, PaginatorInterface $paginator): Response
+    {
+        return $this->redirectToRoute('app_car_admin');
+    }
+
+    #[Route('/admin/delete/{id}', name: 'app_car_remove')]
+    public function delete(Request $request, PaginatorInterface $paginator, Car $car): Response
+    {
+        $this->entityManager->remove($car);
+        $this->entityManager->flush();
+        return $this->redirectToRoute('app_car_admin');
+    }
+
+    #[Route('/admin/update', name: 'app_car_update')]
+    public function update(Request $request, PaginatorInterface $paginator): Response
+    {
+        return $this->redirectToRoute('app_car_admin');
+    }
 }
