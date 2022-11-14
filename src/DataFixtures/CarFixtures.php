22d130c95c10ee3a9c28e8671b1a231de05742d5
fayssal <fayssalmechmeche.pro@gmail.com>
2022-11-14 19:06:21 +0100

    Fast commit

--- src/DataFixtures/CarFixtures.php
+++ src/DataFixtures/CarFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use Faker\Factory;
 use App\Entity\Car;
+use App\Entity\CarCategory;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -11,17 +12,26 @@ class CarFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create();
+
+
+        for ($i = 0; $i < 5; $i++) {
+            $carCategory = new CarCategory();
+            $carCategory->setName($faker->name());
+            $manager->persist($carCategory);
+        }
+        $manager->flush();
 
         for ($i = 0; $i < 20; $i++) {
-            $faker = Factory::create();
             $car = new Car();
+
+
             $number = $faker->randomElement([1, 2, 3, 4]);
             $car->setName($faker->name());
             $car->setNbSeats($number);
             $car->setNbDoors($number);
             $car->setCost($faker->randomDigitNotNull());
+            $car->setCategory($carCategory);
 
 
             $manager->persist($car);
